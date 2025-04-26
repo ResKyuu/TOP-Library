e7d0a60e7cf21e82e2678936e37da0a96c878dba
ResKyuu <73728388+ResKyuu@users.noreply.github.com>
2025-04-26 22:51:15 +0000

    Continued the edit functionality a bit, now able to change the table values but also changing the array values isnt finished yet

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+//Array where all the Books are being stored in
 const myLibrary = [];
+
+//Initializing all the globally needed HTML Elements
 const tbody = document.querySelector("#book-list");
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("#showBookDialog");
 const closeButton = document.querySelector("#cancelBook");
 const addNewBookBtn = document.querySelector("#confirmBook");
+const dialogHeader = document.getElementById("dialogHeader");
 
+//Global variables needed to edit books
+let editMode = false;
+let currentBookId;
+
+//Constructor for all Books
 function Book(id, title, author, pages, available) {
   this.id = id;
   this.title = title;
@@ -13,11 +22,13 @@ function Book(id, title, author, pages, available) {
   this.available = available;
 }
 
+//Function to add all Books to the Library
 function addBookToLibrary(id, title, author, pages, available) {
   const newBook = new Book(id, title, author, pages, available);
   myLibrary.push(newBook);
 }
 
+//Function to display all Books on page load, which exist in the Library Array
 function displayBooks(myLibrary) {
   myLibrary.forEach((Book) => {
     const newRow = tbody.insertRow();
@@ -39,16 +50,22 @@ function displayBooks(myLibrary) {
   });
 }
 
+//Event Listener for "Add New Book" Button
+//Shows Dialog on click of "Add New Book" button
 showButton.addEventListener("click", () => {
+  dialogHeader.innerHTML = "Add New Book";
   dialog.showModal();
 });
 
+//Event Listener for 'Cancel" Button
+//Closes dialog on "Cancel" button click
 closeButton.addEventListener("click", () => {
   dialog.close();
   dialog.querySelector("form").reset();
-  dialog.close();
 });
 
+//Event Listener for the "Confirm" Button
+//On "Confirm" button click of Dialog, Adds/Edits New Book to the Library and displays it
 addNewBookBtn.addEventListener("click", () => {
   const form = dialog.querySelector("form");
   const titleRef = document.querySelector("#title");
@@ -61,37 +78,44 @@ addNewBookBtn.addEventListener("click", () => {
     return;
   }
 
-  const newRow = tbody.insertRow();
-  const bookID = newRow.insertCell();
-  const bookTitle = newRow.insertCell();
-  const bookAuthor = newRow.insertCell();
-  const bookPages = newRow.insertCell();
-  const bookavailable = newRow.insertCell();
+  if (editMode == false) {
+    const newRow = tbody.insertRow();
+    const bookID = newRow.insertCell();
+    const bookTitle = newRow.insertCell();
+    const bookAuthor = newRow.insertCell();
+    const bookPages = newRow.insertCell();
+    const bookavailable = newRow.insertCell();
 
-  const uniqueID = crypto.randomUUID();
-  addBookToLibrary(
-    uniqueID,
-    titleRef.value,
-    authorRef.value,
-    pages.value,
-    availableRef.checked
-  );
+    const uniqueID = crypto.randomUUID();
+    addBookToLibrary(
+      uniqueID,
+      titleRef.value,
+      authorRef.value,
+      pages.value,
+      availableRef.checked
+    );
 
-  bookID.appendChild(document.createTextNode(uniqueID));
-  bookTitle.appendChild(document.createTextNode(titleRef.value));
-  bookAuthor.appendChild(document.createTextNode(authorRef.value));
-  bookPages.appendChild(document.createTextNode(pagesRef.value));
-  console.log(bookavailable.value);
-  bookavailable.appendChild(
-    availableRef.checked
-      ? document.createTextNode("Available")
-      : document.createTextNode("Not Available")
-  );
-  createButtons(newRow, uniqueID);
-  form.reset();
-  dialog.close();
+    bookID.appendChild(document.createTextNode(uniqueID));
+    bookTitle.appendChild(document.createTextNode(titleRef.value));
+    bookAuthor.appendChild(document.createTextNode(authorRef.value));
+    bookPages.appendChild(document.createTextNode(pagesRef.value));
+    console.log(bookavailable.value);
+    bookavailable.appendChild(
+      availableRef.checked
+        ? document.createTextNode("Available")
+        : document.createTextNode("Not Available")
+    );
+    createButtons(newRow, uniqueID);
+    form.reset();
+    dialog.close();
+  } else {
+    editBook(titleRef, authorRef, pagesRef, availableRef);
+    form.reset();
+    dialog.close();
+  }
 });
 
+//Function to Create the "Edit" & "Delete" button to all Books in Table
 function createButtons(row, bookId) {
   const editBook = row.insertCell();
   const deleteBook = row.insertCell();
@@ -107,10 +131,18 @@ function createButtons(row, bookId) {
   editBook.appendChild(editButton);
   deleteBook.appendChild(deleteButton);
 
+  //Event Listener for "Edit" Button
   editButton.addEventListener("click", () => {
-    console.log("Edit button clicked for row:", bookId);
+    //${row.cells[1].textContent} = Title | ${row.cells[2].textContent} = Author
+    dialogHeader.innerHTML = `Edit Book ${row.cells[1].textContent} by ${row.cells[2].textContent}`;
+    editMode = true;
+    console.log("editMode Activated");
+    currentBookId = bookId;
+    dialog.showModal();
   });
 
+  //Event Listener for "Delete" Button
+  //Delete Table row and Library Array entry
   deleteButton.addEventListener("click", () => {
     row.remove(-1);
     const bookLibraryIndex = myLibrary.findIndex((Book) => {
@@ -120,7 +152,6 @@ function createButtons(row, bookId) {
         return false;
       }
     });
-    console.log(bookLibraryIndex);
     myLibrary.splice(bookLibraryIndex, 1);
   });
 }
@@ -129,3 +160,19 @@ addBookToLibrary(crypto.randomUUID(), "test", "test", 20, true);
 addBookToLibrary(crypto.randomUUID(), "test", "test", 20, true);
 addBookToLibrary(crypto.randomUUID(), "test", "test", 20, true);
 displayBooks(myLibrary);
+
+function editBook(title, author, pages, availability) {
+  for (const row of tbody.rows) {
+    if (row.cells[0].textContent == currentBookId) {
+      row.cells[1].innerHTML = title.value;
+      row.cells[2].innerHTML = author.value;
+      row.cells[3].innerHTML = pages.value;
+      row.cells[4].innerHTML = availability.checked
+        ? "Available"
+        : "Not Available";
+    }
+  }
+  //to be done, find the array entry and give it the new values
+  editMode = false;
+  console.log("Edit Mode deactivated");
+}
